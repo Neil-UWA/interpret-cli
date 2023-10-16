8771a6cd9be15e14fa5b1cfa6903d481df6234b4
黑卡 <heika@goluckyvip-inc.com>
2023-10-16 16:05:36 +0800

    ts-nocheck

--- src/cli.ts
+++ src/cli.ts
@@ -15,50 +15,50 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import klaw from 'klaw';
-import {to} from './to';
-import debug from 'debug';
-import {extra} from './ext';
-import Config from './config';
-import program from 'commander';
-import prettier from 'prettier';
-import {Request} from './request';
-import {readFile, writeFile} from 'fs-extra';
+import klaw from "klaw";
+import { to } from "./to";
+import debug from "debug";
+import { extra } from "./ext";
+import Config from "./config";
+import program from "commander";
+import prettier from "prettier";
+import { Request } from "./request";
+import { readFile, writeFile } from "fs-extra";
 
-const log = debug('j2t:cli');
+const log = debug("j2t:cli");
 
 program
-  .version('0.0.1')
-  .usage('-c dubbo.json')
-  .option('-c, --config [value]', 'specify interpret Config ')
+  .version("0.0.1")
+  .usage("-c dubbo.json")
+  .option("-c, --config [value]", "specify interpret Config ")
   .parse(process.argv);
 
 (async () => {
-  const {res: dubboConfig, err: configErr} = await Config.fromConfigPath(
-    program.config,
+  const { res: dubboConfig, err: configErr } = await Config.fromConfigPath(
+    program.config
   );
   if (configErr) {
-    console.error('Error reading configuration file');
+    console.error("Error reading configuration file");
     console.log(configErr);
     log(configErr);
     return;
   }
 
-  const {res: extInfo, err: extError} = await to(extra(dubboConfig));
+  const { res: extInfo, err: extError } = await to(extra(dubboConfig));
   if (extError) {
-    console.error('Failed to extract ast from java class');
+    console.error("Failed to extract ast from java class");
     console.log(extError);
     log(extError);
     return;
   }
 
   //setup jar ast path
-  console.log('read jar ast file', extInfo.jarInfo);
+  console.log("read jar ast file", extInfo.jarInfo);
   dubboConfig.jarInfo = extInfo.jarInfo;
   log(`parse config->${JSON.stringify(dubboConfig, null, 2)}`);
   await new Request(dubboConfig).work();
   await formatSourceDir(dubboConfig.output);
-  console.log('Translation completed');
+  console.log("Translation completed");
 })();
 
 /**
@@ -70,40 +70,40 @@ async function formatSourceDir(srcDir): Promise<void> {
   log(`Format the source code:${srcDir}`);
   return new Promise<void>((resolve, reject) => {
     klaw(srcDir)
-      .on('data', async (item: klaw.Item) => {
-        if (item.path.endsWith('.ts')) {
+      .on("data", async (item: klaw.Item) => {
+        if (item.path.endsWith(".ts")) {
           try {
             let fileContent = await readFile(item.path);
             await writeFile(
               item.path,
-              prettier.format(fileContent.toString(), {
-                parser: 'typescript',
+              prettier.format(`//@ts-nocheck\n` + fileContent.toString(), {
+                parser: "typescript",
                 singleQuote: true,
                 bracketSpacing: false,
-                trailingComma: 'all',
+                trailingComma: "all",
                 semi: true,
-              }),
+              })
             );
             log(`Format the source code successfully:${item.path}`);
           } catch (err) {
             log(`Failed to format the source code:${item.path} ${err}`);
             console.warn(
-              `Failed to format the source code:${item.path} ${err}`,
+              `Failed to format the source code:${item.path} ${err}`
             );
             reject(err);
           }
         }
       })
-      .on('end', () => {
+      .on("end", () => {
         resolve();
       });
   });
 }
 
-process.on('uncaughtException', err => {
+process.on("uncaughtException", (err) => {
   console.log(err);
 });
 
-process.on('unhandledRejection', err => {
+process.on("unhandledRejection", (err) => {
   console.log(err);
 });
